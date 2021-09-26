ab1741870e53dbd2909ef47550364d9fb8db1d4e
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-26 08:15:22 +0200

    using DTO and mapping to domain object

--- service/customerService.go
+++ service/customerService.go
@@ -2,19 +2,20 @@ package service
 
 import (
 	"github.com/rastislavsvoboda/banking/domain"
+	"github.com/rastislavsvoboda/banking/domain/dto"
 	"github.com/rastislavsvoboda/banking/domain/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomers(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetAllCustomers(string) ([]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers(status string) ([]domain.Customer, *errs.AppError) {
+func (s DefaultCustomerService) GetAllCustomers(status string) ([]dto.CustomerResponse, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
@@ -23,11 +24,29 @@ func (s DefaultCustomerService) GetAllCustomers(status string) ([]domain.Custome
 		status = ""
 	}
 
-	return s.repo.FindAll(status)
+	customers, err := s.repo.FindAll(status)
+	if err != nil {
+		return nil, err
+	}
+
+	customerDtos := make([]dto.CustomerResponse, 0)
+	for _, c := range customers {
+		customerDto := c.ToDto()
+		customerDtos = append(customerDtos, customerDto)
+	}
+
+	return customerDtos, nil
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	customer, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	customerDto := customer.ToDto()
+
+	return &customerDto, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
